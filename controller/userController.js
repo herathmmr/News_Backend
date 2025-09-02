022b590636c0a5b7c917cf99e7e6d7846da65fd1
Malshan Herath <mmrherath@students.nsbm.ac.lk>
2025-09-02 14:38:11 +0530

    create user controller

---
+++ controller/userController.js
@@ -0,0 +1,59 @@
+import UserModel from "../models/user.js";
+import bcrypt from "bcrypt";
+import  jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export function registerUser(req, res) {
+    const userData = req.body;
+    userData.password = bcrypt.hashSync(userData.password,10);
+
+    const newUser = new UserModel(userData);
+
+    newUser.save()
+        .then(() => {
+            res.status(201).json({
+                message: "User added successfully"
+            });
+        })
+        .catch((error) => {
+            res.status(500).json({
+                message: "User registration failed",
+                error: error.message
+            });
+        });
+}
+export function loginUser(req,res){
+
+    const userData=req.body;
+    UserModel.findOne({
+    
+        email : userData.email
+
+    }).then((user)=>{
+        if(user==null){
+            res.status(404).json({
+                error:"user not found"
+            })
+        }
+        else{
+            
+            const isPasswordCorrect = bcrypt.compareSync(userData.password,user.password);
+            if(isPasswordCorrect){
+
+                const token = jwt.sign({
+                    firstName : user.firstName,
+                    lastName : user.lastName,
+                    email : user.email,
+                    role : user.role,
+                    profilePicture:user.profilePicture, 
+                    phone : user.phone,      
+                                      },process.env.SECRET_KEY) 
+                res.json({message :" login successfull",token:token});
+            }else{
+                res.status(404).json({error:"login fail"})
+            }
+        }
+    })
+}
